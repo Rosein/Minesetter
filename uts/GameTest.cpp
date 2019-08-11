90f5004899b435739e900f87b334de1a6695816c
Robert <robert.kumanek@gmail.com>
2019-08-11 10:27:29 +0200

    Add ut check half board after one click

--- uts/GameTest.cpp
+++ uts/GameTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "../inc/Game.hpp"
+#include <numeric>
 
 class PrepareBoard : public ::testing::Test
 {
@@ -39,5 +40,37 @@ TEST_F( PrepareBoard, CheckFieldAroundBomb )
     EXPECT_TRUE( game.checkFieldAt( 3, 3 ) );
     EXPECT_EQ( game.getFieldStateAt( 3, 3 ), FieldState::Free );
     EXPECT_EQ( game.getBombCounterAt( 3, 3 ), 1 );    
+
+}
+
+TEST_F( PrepareBoard, CheckHalfBoardAfterOneClick )
+{
+    std::vector<std::pair<int,int>> coords_of_bombs { 10, { 0, 0 } };
+    int i = 0;
+    std::for_each( coords_of_bombs.begin(), coords_of_bombs.end(),[ &i ]( std::pair<int,int> & coords )
+        {
+            coords.first = i;
+            coords.second = i;
+            i++;
+        });
+
+    game.setBombVector( coords_of_bombs );
+    
+    EXPECT_TRUE( game.checkFieldAt( 0, 1 ) );
+    EXPECT_EQ( game.getFieldStateAt( 0, 1 ), FieldState::Free );
+    EXPECT_EQ( game.getBombCounterAt( 0, 1 ), 2 );
+    
+    EXPECT_TRUE( game.checkFieldAt( 0, 2 ) );
+    EXPECT_EQ( game.getFieldStateAt( 0, 2 ), FieldState::Free );
+    EXPECT_EQ( game.getBombCounterAt( 0, 2 ), 0 );
+
+    EXPECT_FALSE( game.checkFieldAt( 0, 3 ) );
+    EXPECT_EQ( game.getFieldStateAt( 0, 3 ), FieldState::Free );
+    EXPECT_EQ( game.getBombCounterAt( 0, 3 ), 0 );
+
+    EXPECT_FALSE( game.checkFieldAt( 5, 6 ) );
+    EXPECT_EQ( game.getFieldStateAt( 5, 6 ), FieldState::Free );
+    EXPECT_EQ( game.getBombCounterAt( 5, 6 ), 2 );
+
 
 }
